6af6dc787ca4e04575ff3a30556e5bed74de99cb
mysd33 <mysd.33@gmail.com>
2026-01-18 08:32:21 +0900

    Linkを使用することの重要性についてのコメント追加

--- components/button/LinkButton.tsx
+++ components/button/LinkButton.tsx
@@ -1,8 +1,8 @@
 // クリック時の処理はクライアントのみで動作するため"use client"を指定
 "use client";
 
-import Link from "next/link";
 import { buildBaseButtonClassName } from "@/components/button/BaseButton";
+import Link from "next/link";
 
 interface Props {
   /**
@@ -51,6 +51,7 @@ export default function LinkButton({
     );
   }
 
+  /* buttonとaタグの違いが出てしまうのため、BaseButtonとclassNameのロジックのみを共通化 */
   const buttonClassName = buildBaseButtonClassName({
     size,
     outline,
@@ -58,9 +59,27 @@ export default function LinkButton({
     className,
   });
 
+  /*
+    aタグやButtonクリックによるRouter.pushを使用してしまうと、ページ全体がリロードされたり、
+    サーバサイドの遷移処理の待ち時間の影響が直接でてしまうため、必ず、Linkを使用する。    
+    Next.jsのLinkコンポーネントを使用して画面遷移を行う
+    https://nextjs.org/learn/dashboard-app/navigating-between-pages
+    https://nextjs.org/docs/app/getting-started/linking-and-navigating
+    
+    これにより、条件にもよるが、リンク先のページがあらかじめプリフェッチ
+    （ユーザーがルートに移動する前にバックグラウンドでルートを読み込む）されるため
+    アプリケーション内のルート間のナビゲーションが瞬時に感じられるようになる。
+    
+    クライアントサイド遷移により、    
+    ページ全体をリロードすることなくlayout等共通のUI部分維持されて、個別のpageのみ遷移させることができる。      
+
+    https://nextjs.org/docs/app/getting-started/linking-and-navigating#client-side-transitions
+  */
   return (
-    <Link href={forwardViewURL} {...linkProps} className={buttonClassName}>
-      {children}
-    </Link>
+    <>
+      <Link href={forwardViewURL} {...linkProps} className={buttonClassName}>
+        {children}
+      </Link>
+    </>
   );
 }
